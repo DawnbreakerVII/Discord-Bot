451020dbfd71dad2ec7f99a3e01d1e54c5714e7a
DawnbreakerVII <137497851+DawnbreakerVII@users.noreply.github.com>
2024-02-29 01:08:14 +0300

    Update Api.py

--- Api.py
+++ Api.py
@@ -1,5 +1,7 @@
 import discord
+import openai
 from discord.ext import commands
+import asyncio
 
 Intents = discord.Intents.default()
 Intents.members = True
@@ -9,3 +11,5 @@ Bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 BotToken = '' #Bot token
 Channel = Numbers #join channel id
 Channel2 = Numbers #exit channel id
+GptToken = '' #OpenAI token
+openai.api_key = GptToken
